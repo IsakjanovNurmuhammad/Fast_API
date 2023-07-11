448a26b39dc0b63459680210d4dc8ee908e0f5ac
vezuch1y <andro3star@gmail.com>
2023-07-11 17:32:48 +0500

    New Init

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-db = []
-
-@app.post("/api/{number_1}/{number_2}")
-async def get_ibnfo(name: str,
-                    l_name: str):
-    n = name.lower()
-    l = l_name.lower()
-    db = {'name': n,
-          "l_name": l}
-    print(db)
-    return {'name':n,
-          "l_name":l}
+BOOKS = [
+    {"title": "Harry Potter", "desc": "smth", "Author": "J.K.Rolling", "price": 150, "id": 1},
+    {"title": "Little", "desc": "smth", "Author": "J.K.Rolling", "price": 150, "id": 2},
+    {"title": "Saga", "desc": "smth", "Author": "J.K.Rolling", "price": 150, "id": 3},
+    {"title": "Attack", "desc": "smth", "Author": "J.K.Rolling", "price": 150, "id": 4}
+]
+@app.get("/api/{title}")
+async def search(title: str):
+    for book in BOOKS:
+        if book.get("title") == title:
+            print(book)
+            return book
+    else:
+        return "Not found"
